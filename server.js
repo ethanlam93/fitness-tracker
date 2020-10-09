b7b7b84ac923972cb70429211decfdc4aae403a4
Ethan Lam <ethanlam93@gmail.com>
2020-10-09 14:23:51 -0400

    deloyment mongoose config

--- server.js
+++ server.js
@@ -13,8 +13,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Connect to Mongoose database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 //Running API and HTML routes
 require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
